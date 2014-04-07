5486b6428d7baf1b5d85f630b671d631ee293a47
fdf <f@ff.ff>
2014-04-07 16:22:53 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java   //产生 HelloWorld.class 文件
+ *  Execution:    java HelloWorld  //HelloWorld  大小写敏感
+ *
+ *  Prints "Hello, World".  
+ 
+ *
+ *************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+ENVRIOMENT:
+windows7 professional sp1
+JAVA
+2014/4/7
